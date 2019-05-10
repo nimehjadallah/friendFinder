9f68f5c64baf122981359d21fba7aac245f40c30
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-05-10 14:50:04 -0400

    pushing all the work I have done locally!

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,44 @@
+// var path = require ('path');
+
+var friendsArray = require('../data/friends.js');
+module.exports = function(app){
+    app.get('/api/friends', function(req, res) {
+        res.json(friendsArray);
+        // console.log(friendsArray);
+});
+
+app.post('/api/friends', function(req,res){
+    //grabs the new friend's scores to compare with friends in friendsArray array
+    var newFriendScores = req.body.scores;
+    var scoresArray = [];
+    var friendCount = 0;
+    var bestMatch = 0;
+
+    //runs through all current friends in list
+    for(var i=0; i<friendsArray.length; i++){
+      var scoresDiff = 0;
+      //run through scores to compare friends
+      for(var j=0; j<newFriendScores.length; j++){
+        scoresDiff += (Math.abs(parseInt(friendsArray[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
+
+      //push results into scoresArray
+      scoresArray.push(scoresDiff);
+    }
+
+    //after all friends are compared, find best match
+    for(var i=0; i<scoresArray.length; i++){
+      if(scoresArray[i] <= scoresArray[bestMatch]){
+        bestMatch = i;
+      }
+    }
+
+    //return bestMatch data
+    var bff = friendsArray[bestMatch];
+    res.json(bff);
+
+    //pushes new submission into the friendsList array
+    friendsArray.push(req.body);
+  });
+
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+
+
+app.listen(PORT, function() {
+    console.log(`Now listening on port:${PORT}`);
+  });
